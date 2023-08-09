969e32fc54d27bdcb1cb05603cf19eed01140566
Ricardo <ricarddodeelcid@gmail.com>
2023-08-08 19:59:01 -0700

    bugfix/change-api-url

--- swapi-client/src/useCases/GetSwapiPeople.ts
+++ swapi-client/src/useCases/GetSwapiPeople.ts
@@ -1,10 +1,10 @@
+import axios from 'axios'
 import { SwapiResponse } from '../components/Home'
-import HttpClient from '../services/HttpClient'
 
 export const getSwapiPeople = (): Promise<SwapiResponse>  => {
   return new Promise(resolve => {
-    HttpClient.get('https://swapi.dev/api/people/').then(response => {
-      resolve(response)
+    axios.get('https://swapi.dev/api/people/').then(response => {
+      resolve(response.data)
     })
   })
 }
